99ade12a75d40fb08302685bd687469cb44941cb
Florian Bruggisser <florian@bildspur.ch>
2018-07-18 11:07:21 +0200

    using new pointer revealing library

--- src/main/kotlin/ch/bildspur/realsense/Sketch.kt
+++ src/main/kotlin/ch/bildspur/realsense/Sketch.kt
@@ -1,8 +1,20 @@
 package ch.bildspur.realsense
 
 import processing.core.PApplet
+import kotlin.math.roundToInt
 
 class Sketch : PApplet() {
+    companion object {
+        @JvmStatic
+        fun map(value: Double, start1: Double, stop1: Double, start2: Double, stop2: Double): Double {
+            return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1))
+        }
+
+        @JvmStatic
+        fun map(value: Int, start1: Int, stop1: Int, start2: Int, stop2: Int): Int {
+            return map(value.toDouble(), start1.toDouble(), stop1.toDouble(), start2.toDouble(), stop2.toDouble()).roundToInt()
+        }
+    }
 
     override fun settings() {
         size(1280, 720)
