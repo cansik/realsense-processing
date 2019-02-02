535dba7d90e04b1db3121268bf4b107e2ba15082
Florian Bruggisser <florian@bildspur.ch>
2019-02-02 16:31:38 +0100

    implemented initial working version of the library

--- src/main/java/ch/bildspur/realsense/RealSenseCamera.java
+++ src/main/java/ch/bildspur/realsense/RealSenseCamera.java
@@ -1,12 +1,12 @@
 package ch.bildspur.realsense;
 
-import org.librealsense.Context;
-import org.librealsense.Device;
-import org.librealsense.DeviceList;
-import org.librealsense.Native;
+import org.librealsense.*;
 import processing.core.PApplet;
 import processing.core.PConstants;
+import processing.core.PImage;
 
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.util.List;
 
 /**
@@ -18,9 +18,22 @@ public class RealSenseCamera implements PConstants {
 
     // realsense
     private Context context;
+    private Pipeline pipeline;
+
+    private final int depthStreamIndex = 0;
+    private final int colorStreamIndex = 0;
 
     // camera
     volatile private boolean running = false;
+
+    private int width;
+    private int height;
+    private int fps;
+    private boolean enableDepthStream;
+    private boolean enableColorStream;
+
+    private PImage depthImage;
+    private PImage colorImage;
 
     /**
      * Create a new Intel RealSense camera.
@@ -40,26 +53,108 @@ public class RealSenseCamera implements PConstants {
 
     /**
      * Start the camera.
+     * @param width Width of the camera image.
+     * @param height Height of the camera image.
+     * @param fps Frames per second to capture.
+     * @param enableDepthStream True if depth stream should be enabled.
+     * @param enableColorStream True if color stream should be enabled.
      */
-     public void start()
+    public void start(int width, int height, int fps, boolean enableDepthStream, boolean enableColorStream)
+    {
+        List<Device> devices = getDevices();
+
+        if(devices.isEmpty())
+            PApplet.println("RealSense: No device found!");
+
+        this.start(devices.get(0), width, height, fps, enableDepthStream, enableColorStream);
+    }
+
+    /**
+     * Start the camera.
+     * @param device Intel RealSense device to start.
+     * @param width Width of the camera image.
+     * @param height Height of the camera image.
+     * @param fps Frames per second to capture.
+     * @param enableDepthStream True if depth stream should be enabled.
+     * @param enableColorStream True if color stream should be enabled.
+     */
+     public void start(Device device, int width, int height, int fps, boolean enableDepthStream, boolean enableColorStream)
      {
          if(running)
              return;
 
+         // set configuration
+         this.width = width;
+         this.height = height;
+         this.fps = fps;
+         this.enableDepthStream = enableDepthStream;
+         this.enableColorStream = enableColorStream;
+
+         // create images
+         this.depthImage = new PImage(this.width, this.height, PConstants.RGB);
+         this.colorImage = new PImage(this.width, this.height, PConstants.RGB);
+
+         // create pipeline
+         pipeline = context.createPipeline();
+
+         Config config = Config.create();
+         config.enableDevice(device);
+         if (this.enableDepthStream) {
+             config.enableStream(Native.Stream.RS2_STREAM_DEPTH,
+                     this.depthStreamIndex,
+                     this.width,
+                     this.height,
+                     Native.Format.RS2_FORMAT_Z16,
+                     this.fps);
+         }
+
+         if (this.enableColorStream) {
+             config.enableStream(Native.Stream.RS2_STREAM_COLOR,
+                     this.colorStreamIndex,
+                     this.width,
+                     this.height,
+                     Native.Format.RS2_FORMAT_RGB8,
+                     this.fps);
+         }
+
+         // start pipeline
+         pipeline.startWithConfig(config);
+
          running = true;
-         setupCamera();
      }
 
     public void readFrames()
     {
+        FrameList frames = pipeline.waitForFrames(5000);
 
+        for (int i = 0; i < frames.frameCount(); i++) {
+            Frame frame = frames.frame(i);
+            if (frame.isExtendableTo(Native.Extension.RS2_EXTENSION_DEPTH_FRAME)) {
+                this.readDepthImage(frame);
+            } else {
+                this.readColorImage(frame);
+            }
+
+            frame.release();
+        }
+
+        frames.release();
     }
 
+    /**
+     * Lists all available devices.
+     * Caution: Opening a device is maybe not possible because of the underlying API.
+     * @return List of Intel RealSense devices.
+     */
     public List<Device> getDevices() {
         DeviceList deviceList = this.context.queryDevices();
         return deviceList.getDevices();
     }
 
+    /**
+     * Returns true if a device is available.
+     * @return True if device is available.
+     */
     public boolean isCameraAvailable() {
         return !getDevices().isEmpty();
     }
@@ -76,9 +171,34 @@ public class RealSenseCamera implements PConstants {
         running = false;
     }
 
-    private void setupCamera()
-    {
+    private void readDepthImage(Frame frame) {
+        CharBuffer buffer = frame.getFrameData().asCharBuffer();
+        this.depthImage.loadPixels();
 
+        for (int i = 0; i < width * height; i++)
+        {
+            int depth = buffer.get(i) & 0xFFFF;
+            int grayScale = (int) PApplet.map(depth, 0, 65536, 255, 0);
+
+            if (depth > 0)
+                depthImage.pixels[i] = parent.color(grayScale);
+            else
+                depthImage.pixels[i] = parent.color(0);
+        }
+
+        this.depthImage.updatePixels();
+    }
+
+    private void readColorImage(Frame frame) {
+        ByteBuffer buffer = frame.getFrameData();
+        this.colorImage.loadPixels();
+
+        for (int i = 0; i < frame.getStrideInBytes() * height; i += 3)
+        {
+            colorImage.pixels[i / 3] = parent.color(buffer.get(i) & 0xFF, buffer.get(i + 1) & 0xFF, buffer.get(i + 2) & 0xFF);
+        }
+
+        this.colorImage.updatePixels();
     }
 
     private void loadNativeLibraries()
@@ -98,10 +218,18 @@ public class RealSenseCamera implements PConstants {
     }
 
     /**
-     * Check if the camera is running.
-     * @return True if the camera is running.
+     * Check if the camera is already running.
+     * @return True if the camera is already running.
      */
     public boolean isRunning() {
         return running;
+    }
+
+    public PImage getDepthImage() {
+        return depthImage;
+    }
+
+    public PImage getColorImage() {
+        return colorImage;
     }
 }
--- src/test/java/ch/bildspur/realsense/test/Sketch.java
+++ src/test/java/ch/bildspur/realsense/test/Sketch.java
@@ -2,6 +2,7 @@ package ch.bildspur.realsense.test;
 
 
 import ch.bildspur.realsense.RealSenseCamera;
+import org.librealsense.Device;
 import processing.core.PApplet;
 import processing.opengl.PJOGL;
 
@@ -9,8 +10,8 @@ import processing.opengl.PJOGL;
  * Created by cansik on 21.03.17.
  */
 public class Sketch extends PApplet {
-    public final static int OUTPUT_WIDTH = 640;
-    public final static int OUTPUT_HEIGHT = 480;
+    public final static int OUTPUT_WIDTH = 1280;
+    public final static int OUTPUT_HEIGHT = 500;
 
     public final static int FRAME_RATE = 60;
 
@@ -24,17 +25,30 @@ public class Sketch extends PApplet {
     public void setup() {
         frameRate(FRAME_RATE);
 
+        /*
         if(camera.isCameraAvailable())
             println("Camera found!");
         else
             println("No camera available!");
+         */
+
+        camera.start(640, 480, 30, true, true);
     }
 
     public void draw() {
         // clear screen
         background(55);
 
-        fill(0, 255, 0);
-        text("FPS: " + frameRate, 20, 20);
+        camera.readFrames();
+
+        // show both streams
+        image(camera.getColorImage(), 0, 0);
+        image(camera.getDepthImage(), 640, 0);
+
+        fill(255, 255, 255);
+        textAlign(LEFT, CENTER);
+        text("Color Stream", 20, 480  + 8);
+        text("Depth Stream", 640 + 20, 480  + 8);
+        surface.setTitle("RealSense Processing - FPS: " + Math.round(frameRate));
     }
 }
