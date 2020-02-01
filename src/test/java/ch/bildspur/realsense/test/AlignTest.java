27435a34f1492b30f9f242fcb0298ecf846da74d
Florian <florian@bildspur.ch>
2020-02-01 23:03:42 +0100

    implemented alignment filter

---
+++ src/test/java/ch/bildspur/realsense/test/AlignTest.java
@@ -0,0 +1,77 @@
+package ch.bildspur.realsense.test;
+
+
+import ch.bildspur.realsense.RealSenseCamera;
+import ch.bildspur.realsense.type.ColorScheme;
+import processing.core.PApplet;
+import processing.opengl.PJOGL;
+
+/**
+ * Created by cansik on 21.03.17.
+ */
+public class AlignTest extends PApplet {
+    public final static int OUTPUT_WIDTH = 1280;
+    public final static int OUTPUT_HEIGHT = 500;
+
+    public final static int VIEW_WIDTH = 640;
+    public final static int VIEW_HEIGHT = 480;
+
+    public final static int FRAME_RATE = 30;
+
+    RealSenseCamera camera = new RealSenseCamera(this);
+
+    public static void main(String... args) {
+        AlignTest sketch = new AlignTest();
+        sketch.runSketch();
+    }
+
+    public void settings() {
+        size(OUTPUT_WIDTH, OUTPUT_HEIGHT, FX2D);
+        PJOGL.profile = 1;
+    }
+
+    public void setup() {
+        frameRate(FRAME_RATE);
+
+        if(camera.isCameraAvailable()) {
+            println("Camera found!");
+        }
+        else {
+            println("No camera available!");
+            exit();
+        }
+
+        camera.enableDepthStream();
+        camera.enableColorStream();
+
+        camera.enableAlign();
+        camera.enableColorizer(ColorScheme.Cold);
+
+        camera.start();
+    }
+
+    public void draw() {
+        // clear screen
+        background(55);
+
+        camera.readFrames();
+
+        // show both streams
+        image(camera.getDepthImage(), 0, 0, VIEW_WIDTH, VIEW_HEIGHT);
+        image(camera.getColorImage(), VIEW_WIDTH, 0, VIEW_WIDTH, VIEW_HEIGHT);
+
+        fill(255, 255, 255);
+        textAlign(LEFT, CENTER);
+        text("Depth Stream", 20, VIEW_HEIGHT  + 8);
+        text("Color Stream", VIEW_WIDTH + 20, VIEW_HEIGHT  + 8);
+        surface.setTitle("RealSense Processing - FPS: " + Math.round(frameRate));
+    }
+
+    @Override
+    public void keyPressed() {
+        super.keyPressed();
+
+        //camera.stop();
+        //System.exit(0);
+    }
+}
