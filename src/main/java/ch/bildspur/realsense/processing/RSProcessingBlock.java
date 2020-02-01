82ef2244e71d33304b90ea6c981ae1e887c62171
Florian <florian@bildspur.ch>
2020-02-01 23:40:41 +0100

    documented processing block

--- src/main/java/ch/bildspur/realsense/processing/RSProcessingBlock.java
+++ src/main/java/ch/bildspur/realsense/processing/RSProcessingBlock.java
@@ -7,17 +7,29 @@ public class RSProcessingBlock<T extends ProcessingBlock> {
     private boolean enabled;
     private T block;
 
+    /**
+     * Initialize a new processing block.
+     * @param block Block to initialize.
+     */
     public void init(T block) {
         this.block = block;
 
         this.enabled = true;
     }
 
+    /**
+     * Release the current block.
+     */
     public void release() {
         if(enabled)
             block.release();
     }
 
+    /**
+     * Set option for block.
+     * @param option Option to be set.
+     * @param value Value to be set.
+     */
     public void setOption(Option option, float value) {
         if(!enabled)
             throw new RuntimeException("Block has not been enabled!");
@@ -25,6 +37,11 @@ public class RSProcessingBlock<T extends ProcessingBlock> {
         block.getOptions().get(option).setValue(value);
     }
 
+    /**
+     * Reads value from block option.
+     * @param option Option to read from.
+     * @return Actual value of block.
+     */
     public float getValue(Option option) {
         if(!enabled)
             throw new RuntimeException("Block has not been enabled!");
@@ -32,10 +49,18 @@ public class RSProcessingBlock<T extends ProcessingBlock> {
         return block.getOptions().get(option).getValue();
     }
 
+    /**
+     * Returns true if block has been enabled.
+     * @return Returns true if block has been enabled.
+     */
     public boolean isEnabled() {
         return enabled;
     }
 
+    /**
+     * Returns current block.
+     * @return Returns current block.
+     */
     public T getBlock() {
         return block;
     }
