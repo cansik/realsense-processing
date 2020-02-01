24e0ec8cd8613388ec6e9598bf418acce30e4300
Florian <florian@bildspur.ch>
2020-02-01 21:36:40 +0100

    implemented getters for data and images

--- src/main/java/ch/bildspur/realsense/stream/VideoRealSenseStream.java
+++ src/main/java/ch/bildspur/realsense/stream/VideoRealSenseStream.java
@@ -6,12 +6,16 @@ import processing.core.PConstants;
 import processing.core.PImage;
 
 public class VideoRealSenseStream extends RealSenseStream {
-    private PImage image;
+    private PImage image = null;
 
     @Override
     public void init(Stream streamType, int index, int width, int height, Format format, int fps) {
         super.init(streamType, index, width, height, format, fps);
 
         image = new PImage(width, height, PConstants.RGB);
+    }
+
+    public PImage getImage() {
+        return image;
     }
 }
