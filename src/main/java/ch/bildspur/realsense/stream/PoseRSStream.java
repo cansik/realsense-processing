24ce961934427c2f5e2483ca690522ed4c2d9abe
Florian Bruggisser <florian@bildspur.ch>
2021-01-19 14:43:29 +0100

    added comment for pose

--- src/main/java/ch/bildspur/realsense/stream/PoseRSStream.java
+++ src/main/java/ch/bildspur/realsense/stream/PoseRSStream.java
@@ -6,6 +6,7 @@ public class PoseRSStream extends RSStream {
     private Pose pose;
 
     public void copyPose(Pose raw) {
+        // todo: make a PVector / PQuaternion Pose out of it
         pose = raw;
     }
 
